341169286cbc64e2952a325cdde669f727f030cb
Casey Bartman <bartman.26@osu.edu>
2025-10-06 18:53:02 -0400

    Implemented Purchase page

---
+++ game-start/src/StatePayload.js
@@ -0,0 +1,17 @@
+export const StatePayload = {
+    credit_card_data: {
+        card_number: "",
+        expir_date: "", 
+        cvvCode: "", 
+    },
+
+    shipping_address:  {
+        address_1: "",
+        address_2: "",
+        city: "",
+        state: "",
+        zip: "",
+    },
+
+  buyQuantity: [0, 0, 0, 0, 0]
+};
--- game-start/src/components/purchase.js
+++ game-start/src/components/purchase.js
@@ -1,14 +1,45 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { StatePayload } from "../StatePayload";
 
-//Develop purchase.js to allow collect user requested quantity
-const purchase = () => {
+const Purchase = () => {
+  const [order, setOrder] = useState(StatePayload);
+  const navigate = useNavigate(); //This is our hook for React
   let title = "purchase page";
+
+  //This action handler is used to route to our next page
+  const handleSubmit = (e) => {
+    navigate("/purchase/paymentEntry", { order: order, setOrder: setOrder });
+  };
+
+  console.log("order: ", order);
 
   return (
     <div>
-      <h1>{title}</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Product 1</label>
+        <input
+          type="number"
+          required
+          onChange={(e) => {
+            order.buyQuantity[0] = e.target.value;
+          }}
+        />
+        <br />
+        <label>Product 2</label>
+        <input
+          type="number"
+          required
+          onChange={(e) => {
+            order.buyQuantity[1] = e.target.value;
+          }}
+        />
+        <br />
+        <button className="button">Pay</button>
+      </form>
     </div>
   );
 };
 
-export default purchase;
+export default Purchase;
