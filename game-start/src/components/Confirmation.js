0ca942a1528729bdf7b9c4135eeb14cb3d73e256
Casey Bartman <bartman.26@osu.edu>
2025-10-06 18:32:06 -0400

    Added base routing for urls

---
+++ game-start/src/components/Confirmation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Confirmation = () => {
+  let title = "Confirmation Page";
+
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+export default Confirmation;
---
+++ game-start/src/components/purchase.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Develop purchase.js to allow collect user requested quantity
+const purchase = () => {
+  let title = "purchase page";
+
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+export default purchase;
